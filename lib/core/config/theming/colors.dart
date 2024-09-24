f1721d30d563da955700cc6ad157035624dbac67
HusamAwad2001 <www.hosamawad2016@gmail.com>
2024-09-24 14:43:48 +0300

    add app colors in (ColorsManager)

--- lib/core/config/theming/colors.dart
+++ lib/core/config/theming/colors.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
 abstract class ColorsManager {
-  static const Color primaryColor = Color(0xFF247CFF);
-  static const Color mainBlue = Color(0xFF247CFF);
-  static const Color lightBlue = Color(0xFFF4F8FF);
-  static const Color darkBlue = Color(0xFF242424);
-  static const Color gray = Color(0xFF757575);
-  static const Color lightGray = Color(0xFFC2C2C2);
-  static const Color lighterGray = Color(0xFFEDEDED);
-  static const Color moreLightGray = Color(0xFFFDFDFF);
-  static const Color moreLighterGray = Color(0xFFF5F5F5);
+  static const Color primaryColor = Color(0xFFFF1768);
+  static const Color mainRed = Color(0xFFDD2222);
+  static const Color mainGreen = Color(0xFF2F8655);
+  static const Color mainBlue = Color(0xFF4285F4);
+  static const Color greyD9 = Color(0xFFD9D9D9);
+  static const Color grey79 = Color(0xFF797979);
+  static const Color grey55 = Color(0xFF555555);
+  static const Color grey7E = Color(0xFF7E7E7E);
+  static const Color greyEC = Color(0xFFECECEC);
+  static const Color greyFC = Color(0xFFFCFCFC);
 }
