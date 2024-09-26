c19c9ea11f6c3deb2cf4275495a87801e59609b9
HusamAwad2001 <www.hosamawad2016@gmail.com>
2024-09-26 16:13:04 +0300

    update color variable name

--- lib/core/config/theming/colors.dart
+++ lib/core/config/theming/colors.dart
@@ -4,7 +4,7 @@ abstract class ColorsManager {
   static const Color primaryColor = Color(0xFFFF1768);
   static const Color transparent = Colors.transparent;
   static const Color white = Color(0xFFFFFFFF);
-  static const Color lightWhite = Color(0xFFFEEBF7);
+  static const Color lightPrimary = Color(0xFFFEEBF7);
   static const Color whiteF9 = Color(0xFFF9F9F9);
   static const Color black = Color(0xFF000000);
   static const Color mainRed = Color(0xFFDD2222);
