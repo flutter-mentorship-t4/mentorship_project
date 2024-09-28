ce95088d4dd4a17c6b7914703bb33f53f9633480
Nizar-ST <nizarzayto@gmail.com>
2024-09-28 23:33:22 +0100

    feat(productDetails) :rocket: added product title and product review widgets

--- lib/features/product_details/ui/product_details_screen.dart
+++ lib/features/product_details/ui/product_details_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mentorship_project/core/config/theming/styles.dart';
 import 'package:mentorship_project/features/product_details/data/models/dummy_product.dart';
+import 'package:mentorship_project/features/product_details/ui/widgets/product_rating.dart';
 
 import 'widgets/product_details_top_bar.dart';
 import 'widgets/product_images_carousel.dart';
@@ -28,29 +31,31 @@ class ProductDetailsScreen extends StatelessWidget {
               ProductDetailsTopBar(),
               // Text(LocaleKeys.ProductDetailsScreen.tr()), // Using the generated key and .tr() for translation
             ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      constraints: BoxConstraints(maxWidth: 220.w),
+                      child: Text(
+                        product.title,
+                        style: TextStyles.font16Grey55Regular,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    //review
+                    ProductRating(product: product),
+                  ],
+                ),
+              ],
+            ),
           )
         ],
       ),
     );
   }
 }
-
-// Usage example:
-// void main() {
-//   runApp(MaterialApp(
-//     home: ProductDetailsScreen(
-//       product: Product.fromJson({
-//         "id": 1,
-//         "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-//         "price": 109.95,
-//         "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-//         "category": "men's clothing",
-//         "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-//         "rating": {
-//           "rate": 3.9,
-//           "count": 120
-//         }
-//       }),
-//     ),
-//   ));
-// }
