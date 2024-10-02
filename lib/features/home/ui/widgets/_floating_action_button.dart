1d88304ff908146a954df83b21a748f0cd49b980
Husam Dahliz <husamdahliz@gmail.com>
2024-10-02 20:59:45 +0300

    feat(Home) ♻️ Extract floating action button to external class

---
+++ lib/features/home/ui/widgets/_floating_action_button.dart
@@ -0,0 +1,17 @@
+part of '../home_screen.dart';
+
+class _FloatingActionButton extends StatelessWidget {
+  const _FloatingActionButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.small(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(100),
+      ),
+      backgroundColor: ColorsManager.primaryColor,
+      child: Icon(Icons.add, color: Colors.white, size: 20.w),
+      onPressed: () {},
+    ).widthAndHeight(width: 40.w, height: 40.h);
+  }
+}
