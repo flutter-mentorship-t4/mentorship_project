426c510adbf2c3c684369371927c622eefdfbad4
Nizar-ST <nizarzayto@gmail.com>
2024-10-19 12:15:39 +0100

    feat(chekout) :rocket: linked the ui with the product details screen

--- lib/features/product_details/ui/widgets/product_action_button.dart
+++ lib/features/product_details/ui/widgets/product_action_button.dart
@@ -1,6 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mentorship_project/core/helpers/extensions/navigations_extensions.dart';
+import 'package:mentorship_project/core/routing/routes.dart';
 import 'package:mentorship_project/features/wishlist/logic/cubit/wishlist_cubit.dart';
 
 import '../../../../core/config/theming/colors.dart';
@@ -44,7 +46,9 @@ class ProductActionButtons extends StatelessWidget {
             children: [
               AppButton(
                 label: LocaleKeys.ProductDetailsScreen_BuyNow.tr(),
-                onTap: () {},
+                onTap: () {
+                  context.pushNamed(Routes.checkoutScreen);
+                },
                 borderRadius: 24,
                 fontSize: 18,
                 width: 155,
